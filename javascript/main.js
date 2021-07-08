6d4b96ac527bdac502453ac0d60896fbd9c96531
Martin Fuentes <mefuentes@uninorte.edu.co>
2021-07-08 16:09:13 -0500

    [ADD] Play-pause animation to circular progress bar

--- javascript/main.js
+++ javascript/main.js
@@ -15,11 +15,12 @@
 
     const updatePlayButton = () => {
         playButton();
-    }
+    };
 
     playButton = () => {
         var elems = document.getElementsByClassName("inner-circle");
         var btn = document.getElementById("play");
+        var progressBar = document.getElementsByClassName("circularProgressBar");
 
         for (var i = 0; i < elems.length; i++) {
             elems[i].onclick = function () {
@@ -32,16 +33,24 @@
                     this.style.border = 'var(--light-gray) solid 5px';
                     this.style.boxShadow = '-5px -5px 15px var(--light-color), 5px 5px 15px var(--shadow-color), inset -3px -3px 10px var(--light-color), inset 3px 3px 10px var(--shadow-color)';
                     btn.innerHTML = '<img src="./source/stop.svg" alt="Play button">';
-                    
+
+                    for (var i = 0; i < progressBar.length; i++) { 
+                        progressBar[i].style.animation ='animate 1s linear infinite';
+                    }
+
                 } else {
                     this.style.background = "rgb(230, 248, 222)";
                     this.style.border = 'transparent solid 5px';
                     this.style.boxShadow = '';
                     btn.innerHTML = '<img src="./source/play.svg" alt="Play button">';
-                }
+
+                    for (var i = 0; i < progressBar.length; i++) { 
+                        progressBar[i].style.animation ='';
+                    }
+                };
             };
-        }    
-    }
+        };
+    };
 
     getCurrentTime = () => {
         let fullTime = new Date();
@@ -56,7 +65,7 @@
             minutes: minutes,
             seconds: seconds
         };
-    }
+    };
 
     displayCurrentTime = () => {
         let hours = window.clock.time.hours
